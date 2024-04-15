40f0c00ebc3f133fd567bffc3fab09e644d86154
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-15 21:40:27 +0900

    set activity to compose

--- app/src/main/java/com/jhj0517/android_template_mvvm_compose/MainActivity.kt
+++ app/src/main/java/com/jhj0517/android_template_mvvm_compose/MainActivity.kt
@@ -2,19 +2,32 @@ package com.jhj0517.android_template_mvvm_compose
 
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.jhj0517.android_template_mvvm_compose.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.jhj0517.android_template_mvvm_compose.ui.Empty_activity_sampleTheme
+import com.jhj0517.android_template_mvvm_compose.views.Greeting
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        enableEdgeToEdge()
+        setContent {
+            Empty_activity_sampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
     }
 }
+
