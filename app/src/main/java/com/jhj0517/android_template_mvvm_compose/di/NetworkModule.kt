7c35515078ef257f3c1816d9efa21cd668ff8752
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-16 19:33:01 +0900

    rename

--- app/src/main/java/com/jhj0517/android_template_mvvm_compose/di/NetworkModule.kt
+++ app/src/main/java/com/jhj0517/android_template_mvvm_compose/di/NetworkModule.kt
@@ -2,7 +2,7 @@ package com.jhj0517.android_template_mvvm_compose.di
 
 import com.jhj0517.android_template_mvvm_compose.BuildConfig
 import com.jhj0517.android_template_mvvm_compose.network.BinanceService
-import com.jhj0517.android_template_mvvm_compose.network.Client
+import com.jhj0517.android_template_mvvm_compose.network.ClientFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,7 +16,7 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideBinanceService(): BinanceService {
-        return Client.create(baseUrl = BuildConfig.BinanceBaseURL, service = BinanceService::class.java)
+        return ClientFactory.create(baseUrl = BuildConfig.BinanceBaseURL, service = BinanceService::class.java)
     }
 
 }
